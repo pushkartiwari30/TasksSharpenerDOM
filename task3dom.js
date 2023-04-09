843e449a282c439243db6af695c1fcd273e558a0
pushkartiwari30 <129843395+pushkartiwari30@users.noreply.github.com>
2023-04-10 02:26:59 +0530

    Task3DOM

---
+++ task3dom.js
@@ -0,0 +1,9 @@
+document.title='It is my Title';
+var headerTitle= document.getElementById("header-title");
+var header= document.getElementById("main-header");
+headerTitle.textContent="Shopping Zone";
+header.style.borderBottom='Solid 3px #000';
+
+var title= document.getElementsByClassName("title")[0];
+title.style.color='green';
+title.style.fontWeight='bold';
