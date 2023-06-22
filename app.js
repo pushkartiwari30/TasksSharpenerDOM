51f4640a0644a7f091f8b7eae1691283e255f3be
pushkartiwari30 <129843395+pushkartiwari30@users.noreply.github.com>
2023-06-22 08:16:38 +0530

    ExpressJsTask2

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
-//const http = require('http'); // this is included in app.listen(3000) method internally 
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express();  // th express exports a fucntion hence we store the 'express()' in 'app' 
-// a lot of logic is in the const 'app'
+const app = express();  
 
-app.use((req, res, next)=>{
-    console.log("In the middleware!");
-    next(); // Allows the request to continue to the next middleware in line
-}) // 'use' is defined by the express framework.'use' method allows us to add a new middleware fn
-//the cb fn inside the use method will be executed for all the request.
-// next is another funtion tht we pass in the cb fn as a parameter
-// next is executed to allow the request to go into next middleware
-// we should call next () if we dont send a response otherwise the rewuest will just die and wil not continue to the next middleware
-app.use((req, res, next)=>{
-    console.log("In the another middleware!");
-    // next()  // if there was another function below this
-    res.send('<h1>Hello From Express</h1>');  // in send we can pass in any data type . Express will automatically read the type without need to  specifying
-    //response is send in the last middleware. 
+// the parsing of the body ahould be done before all the routing code 
+// for this to work we need to install a 3rd Party Package into production dependency: body-parser   
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use('/',(req, res, next)=>{
+    //console.log("This always runs for any URL");
+    next();
+});
+app.use('/add-product',(req, res, next)=>{
+    //console.log("In the another middleware!");
+    res.send('<html><h2>Enter the Product Name and the Size</h2><form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>'); 
+});
+// the above middle ware will be executed if url of the browser is : http://localhost:3000/add-product
+// action tell the path(url) to whcih the request is to be send 
+
+app.post('/product', (req,res,next)=>{
+    //getting the body of my incoming  request by extrcting what the user sent me 
+    console.log(req.body); //getting body of a request directly is a new feature of express. 
+    //But using this functionality the rquest does not by default parse the incoming request body .So we need a parser 
+
+    // we need to redirect (here log the data to the console )
+    res.redirect('/');
 })
-// Note: For the next() fn to execute the next function  that funtion should have 3 parameter format with last parameter is the next fn itself. 
-// if we dont use next then the 2nd 'app.use' would nto have been executed. 
 
-// const server = http.createServer(app); // the app is  avalid request handler 
-// server.listen(3000);
+// app.get == app.use but app.get() will only be fired for a GET request 
+// app.post == app.use but app.post() will only be fired for a POST request
+// we also have app.delete/patch/put 
 
-//Instead of above 2 line of code we can write the followoing 
+//Actual format of app.use ==>  app.use([path,]callback[,callback...])
+app.use('/',(req, res, next)=>{
+    //console.log("In the another middleware!");
+    res.send('<h1>Hello From Express</h1>'); 
+});
+
+
+
+// refering to the above middelware , the path '/' signifies anypath starting from '/' . So for node '/' and '/addproduct'are same
 app.listen(3000);
